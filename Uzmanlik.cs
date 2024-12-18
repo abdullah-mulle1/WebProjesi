bf0865ba366fc293631d79461413b873908260b8
abdullah-mulle1 <133582938+abdullah-mulle1@users.noreply.github.com>
2024-12-18 21:21:54 +0300

    Add files via upload

---
+++ Uzmanlik.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Berber.Models
+{
+    public class Uzmanlik
+    {
+        public int Id { get; set; } // المفتاح الأساسي
+
+        [Required]
+        public string Ad { get; set; } // اسم التخصص
+
+        public int CalisanId { get; set; } // المفتاح الأجنبي
+        public Calisan Calisan { get; set; } // العلاقة مع كيان Calisan
+    }
+}
