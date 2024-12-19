98c44062d9d666ddce46acf7c6be48cbdaf79677
abdullah-mulle1 <133582938+abdullah-mulle1@users.noreply.github.com>
2024-12-19 23:53:45 +0300

    Add files via upload

---
+++ Randevu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Berber.Models
+{
+    public enum OnayDurumu { Bekliyor, Onaylandi, Reddedildi }
+    public class Randevu
+    {
+        public int Id { get; set; }
+
+       // [Required]
+        public int ClsnId { get; set; } // Çalışan ID
+        public Calisan Clsn { get; set; } // Çalışan
+
+       // [Required]
+        public int HzmtrId { get; set; } // Hizmet ID
+        public Hizmet Hzmtr { get; set; } // Hizmet
+
+       // [Required]
+        public DateTime RndvSt { get; set; } // Randevu Saati
+
+        public OnayDurumu Onay { get; set; } = OnayDurumu.Bekliyor; // Durum: Kullanılıyor mu?
+    }
+}
