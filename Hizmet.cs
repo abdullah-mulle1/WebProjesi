c18b0aafada4caf6ce11daae3771293259558526
abdullah-mulle1 <133582938+abdullah-mulle1@users.noreply.github.com>
2024-12-22 21:04:48 +0300

    Add files via upload

---
+++ Hizmet.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Berber.Models
+{
+    public class Hizmet
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Ad { get; set; } // Hizmet Adı
+        [Required]
+        public decimal Ucrt { get; set; } // Ücret
+        public int SrDk { get; set; } // Süre (dakika)
+    }
+}
