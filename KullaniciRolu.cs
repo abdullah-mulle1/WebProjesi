eb63105b9c30c67959755de25588db053c46fde1
abdullah-mulle1 <133582938+abdullah-mulle1@users.noreply.github.com>
2024-12-24 23:37:21 +0300

    Add files via upload

---
+++ KullaniciRolu.cs
@@ -0,0 +1,51 @@
+﻿using Berber.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace Berber.Data
+{
+    public static class KullaniciRolu
+    {
+        public static async Task InitializeAsync(IServiceProvider hizmetSaglayici)
+        {
+            var rolYoneticisi = hizmetSaglayici.GetRequiredService<RoleManager<IdentityRole>>();
+            var kullaniciYoneticisi = hizmetSaglayici.GetRequiredService<UserManager<Kullanici>>();
+
+            string[] rolIsimleri = { "Admin", "Calisan", "Kullanici" };
+            IdentityResult rolSonucu;
+
+            foreach (var rol in rolIsimleri)
+            {
+                var rolVarMi = await rolYoneticisi.RoleExistsAsync(rol);
+                if (!rolVarMi)
+                {
+                    rolSonucu = await rolYoneticisi.CreateAsync(new IdentityRole(rol));
+                }
+            }
+
+            var adminEposta = "b211210577@sakarya.edu.tr";
+            var adminSifre = "sau";
+
+
+            // Kontrol: Admin kullanıcısı zaten var mı?
+            if (await kullaniciYoneticisi.FindByEmailAsync(adminEposta) == null)
+            {
+                var adminKullanici = new Kullanici
+                {
+                    Email = adminEposta,
+                    UserName = adminSifre,
+                     EmailConfirmed = true
+                };
+
+                var sonuc = await kullaniciYoneticisi.CreateAsync(adminKullanici, adminSifre);
+                if (sonuc.Succeeded)
+                {
+                    // Admin rolü ata
+                    await kullaniciYoneticisi.AddToRoleAsync(adminKullanici, "Admin");
+                }
+            }
+        }
+    }
+}
+
